be923c1e009d65e5bb75bd97afa0b6d359708397
Eric Mann <eric@eamann.com>
2013-06-08 16:55:04 -0700

    Fix bugs related to removing SASS/LESS support

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -62,23 +62,6 @@ module.exports = function( grunt ) {
 		test:   {
 			files: ['assets/js/test/**/*.js']
 		},
-		{% if ('sass' === css_type) { %}
-		sass:   {
-			all: {
-				files: {
-					'assets/css/{%= js_safe_name %}.css': 'assets/css/sass/{%= js_safe_name %}.scss'
-				}
-			}
-		},
-		{% } else if ('less' === css_type) { %}
-		less:   {
-			all: {
-				files: {
-					'assets/css/{%= js_safe_name %}.css': 'assets/css/less/{%= js_safe_name %}.less'
-				}
-			}		
-		},
-		{% } %}
 		cssmin: {
 			options: {
 				banner: '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
@@ -89,35 +72,13 @@ module.exports = function( grunt ) {
 			},
 			minify: {
 				expand: true,
-				{% if ('sass' === css_type || 'less' === css_type) { %}
-				cwd: 'assets/css/',				
-				src: ['{%= js_safe_name %}.css'],
-				{% } else { %}
 				cwd: 'assets/css/src/',
 				src: ['{%= js_safe_name %}.css'],
-				{% } %}
 				dest: 'assets/css/',
 				ext: '.min.css'
 			}
 		},
 		watch:  {
-			{% if ('sass' === css_type) { %}
-			sass: {
-				files: ['assets/css/sass/*.scss'],
-				tasks: ['sass', 'cssmin'],
-				options: {
-					debounceDelay: 500
-				}
-			},
-			{% } else if ('less' === css_type) { %}
-			less: {
-				files: ['assets/css/less/*.less'],
-				tasks: ['less', 'cssmin'],
-				options: {
-					debounceDelay: 500
-				}
-			},
-			{% } else { %}
 			styles: {
 				files: ['assets/css/src/*.css'],
 				tasks: ['cssmin'],
@@ -125,7 +86,6 @@ module.exports = function( grunt ) {
 					debounceDelay: 500
 				}
 			},
-			{% } %}
 			scripts: {
 				files: ['assets/js/src/**/*.js', 'assets/js/vendor/**/*.js'],
 				tasks: ['jshint', 'concat', 'uglify'],
@@ -141,21 +101,10 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	{% if ('sass' === css_type) { %}
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	{% } else if ('less' === css_type) { %}
-	grunt.loadNpmTasks('grunt-contrib-less');
-	{% } %}
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	
 	// Default task.
-	{% if ('sass' === css_type) { %}
-	grunt.registerTask( 'default', ['jshint', 'concat', 'uglify', 'sass', 'cssmin'] );
-	{% } else if ('less' === css_type) { %}
-	grunt.registerTask( 'default', ['jshint', 'concat', 'uglify', 'less', 'cssmin'] );
-	{% } else { %}
 	grunt.registerTask( 'default', ['jshint', 'concat', 'uglify', 'cssmin'] );
-	{% } %}
 
 	grunt.util.linefeed = '\n';
 };
--- root/index.php
+++ root/index.php
@@ -19,7 +19,7 @@ get_header(); ?>
 
 			<?php if ( have_posts() ) : ?>
 
-				<?php {%= prefix }_content_nav( 'nav-above' ); ?>
+				<?php {%= prefix %}_content_nav( 'nav-above' ); ?>
 
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
